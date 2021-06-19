e45a16e1dea8f2233d41797ac51e6744a988e768
leeping-ng <leeping_ng@hotmail.com>
2021-06-19 11:49:59 +0800

    removed old template files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Header from './Components/Header';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Footer from './Components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <About />
-      <Projects />
-      <Footer />
     </div>
   );
 }
