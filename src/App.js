0e531631f006bbe3462c894efe867a9307e0a8a1
leeping-ng <leeping_ng@hotmail.com>
2021-10-10 20:06:37 +0800

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from "react-ga4";
 import { useEffect } from 'react';
 import { HashRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
   
@@ -24,6 +25,9 @@ function App() {
     </Router>
   );
 }
+
+ReactGA.initialize('G-QDLJZWGBZS');
+ReactGA.send("pageview");
 
 function ScrollToTop() {
   const { pathname } = useLocation();
