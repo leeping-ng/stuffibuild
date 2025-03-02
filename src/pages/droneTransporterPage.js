a940b1cf2a3ad494c842562d5971092a4a57d650
leeping-ng <leeping_ng@hotmail.com>
2025-03-02 20:47:51 +0000

    update image urls for drone transporter and tower crane

--- src/pages/droneTransporterPage.js
+++ src/pages/droneTransporterPage.js
@@ -4,19 +4,19 @@ import TemplatePage from './templatePage';
 import Icon from '@material-ui/icons/DriveEta';
 
 function droneTransporterPage() {
-	
+
 	return (
 		<React.Fragment>
-			<TemplatePage 
-				pageName={pageName} 
-				creationDate={creationDate} 
-				creationDescr={creationDescr} 
-				creationChips={creationChips} 
-				info={info} 
-				icon={<Icon fontSize="large"/>} />
+			<TemplatePage
+				pageName={pageName}
+				creationDate={creationDate}
+				creationDescr={creationDescr}
+				creationChips={creationChips}
+				info={info}
+				icon={<Icon fontSize="large" />} />
 		</React.Fragment>
 	);
-	}
+}
 
 export default droneTransporterPage;
 
@@ -34,91 +34,91 @@ const info = [
 	{
 		name: "Display",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mBUYLWY5yANKcvuQQLWxxr_IAbTTcEu37LrNTIFsCbc0GYB1BUDZQKLyu7MV5uKWI7Qx2BhORIRRjmH5qlFAdsSbVdDKnx3eLoozI-eBfnLO6_tTuaSa-fMN-M8_BSCuhNjWLutHpVTEvbDU4Pk1nQaBllA7w_Z3JfA5-p8LZI6gEU4kYT4zzmmoYcqehp-gg?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIqSQAAAAAAMY5cvIfsQa3_Tw",
 		descr: "The drone is ready to be launched!"
 	},
-    {
+	{
 		name: "Front View",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mMhnAOqszWlwcwM4Q4KXAaCimywEBZ8CCR82GVUd_byK-uZCDxS6DlWeK6EdSW25_ZD-VzFLc4mP1ygzMn0TNOP5EBHXBAuteyQCe8l13Nc2RDt2-npHQY8AFKZXs5JbVa-Lwn6FgwkylX7rSVDMrOSNoNicqct_igC6TZ5Onrozy1CZvHKYfy0d2aeEahwiN?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIqCQAAAAAAGt4YBXLU4nGQuY",
 		descr: "I fitted a huge silver Mercedes-Benz three-pointed star in the front of the truck. I used a rather unconventional approach - this part is actually a wheel rim with tire removed."
 	},
-    {
+	{
 		name: "Rims",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mRVDGsBsuQl5n3qHZO_R2Vuml2Y14FkuBq1eVPCBNLZwMkusWOyoqWinMFMLF70mQIftoJcEQjOM3Qy9LXKl99su1XBQvrhwSfp0zOEMzAZz5vysJx5NfYlLi6TXWjcLnynPoOsBmpQLeXdnI-snL5aWBafa5vDOJZBM95ut_7-6ytTCXHxQrMZn9KH2PTo4z?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICItSQAAAAAAFrXeXsSLaNYNbc",
 		descr: "The same rim was used for all 4 wheels."
 	},
 	{
 		name: "Headlights",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mT3DGTbZY4a4Ym-jEHG-avHDgzB3xKc524nAZ1iRZEmD0AprXAI6mL0KTr8gKTzaBpaSvygSTR8PuHUI_il89f_a9PlMuegda4iS5uw2oLFxSBIwEZcC37eQFEOMJ7ff-1kGqM56CITB-ubiSzsAbLJPyXir7ulmaic-mx_jr2E0jSPOGmAQFJf1Apzi2z4Du?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIqiQAAAAAAJ4NGTcRqSYqXCY",
 		descr: "Head lights."
 	},
 	{
 		name: "Tail Light",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mKqOfMa3X87p14FZetj_vvlgut0RtzSbXred8vJhqMA2VqC3Fiyi13JHZM9sxqomJ6YyyyqvTsHOKLbQJxyWuMLu4GR_UC0dI7GrnHXmF7kzwlev_MajrrPG2R2aMykyb6BjSQac87giz6mu_EjNT16NSwVYMqSyKk-qPiPvh7qWT2IdeGv0bwbrjlhYejhgZ?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIrCQAAAAAAKWmTbAJ8wblPbk",
 		descr: "Tail lights."
 	},
 	{
 		name: "Side View",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mu0Gs2p8WFFfpVvOn1VY7o3bAtUOP_3hZcNQSZzCMNTXA4fMe2y6M43tNCKeriOhBj03EA--wq3F4CWbCXVBzaUHMuGGw8qiCNqVFYam35htpaGMvoQEasEYzv_sTNO4E8PHWKR-f1zU-Byglq1gvIgKGxf8c_a09V915ns1ysj4vmuC1u-w8ZLIF8FMTaLGE?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIqyQAAAAAAHryS-QNxz1vpGg",
 		descr: "Side view, with the drone stowed."
 	},
 	{
 		name: "Top View",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mmdopqM3lP6g9UufUhn4IL9O9HdpSLuIjuzmDFN43BNlcHmj1DtfKTNA2YKx2DH1JEkdNGomMtd979HL76XKAu7h-C7tXt2Dv4SItmyFNPXCXYcmVdZZOire6NLyD-6_Y_6h9y8zC_Y-pbWYGfnlwYtvRxbBfunDW6mjXy-N9ZwENKjTMP_FeLS5cQGmTwDqD?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIrSQAAAAAAPfwWin2ClNfsZE",
 		descr: "Drone ready for launch."
 	},
 	{
 		name: "Hidden Controls",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mYtKVb-kzViCPYkHa06tjb4aDZGZyRRV5--iAOyiop-bDbzRSX18q_1PD49BEACTil-EQNe4koj0Pd7ngGeCWglWYY_9XbspsDlFxUvvLsOmAlSsoPdr9ivOVISCUAi7f-qnOv3lqs0YEGtLr70wEANTrJDSJnNgzF59BvH4dSDgRHpN-NGVtdqpt--HlDjb_?width=4128&height=3084&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIriQAAAAAALzzQCpcQoL8Abc",
 		descr: "The on/off switch for the battery box and the 4 levers for the gearbox are all hidden from plain sight by default."
 	},
 	{
 		name: "Drone Deployment",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mQYCrLjt_H8h_jRChBPq30bYgf5ZHptWHh_XTReDgFDIylErgz6c1WPWOD-p3538hkcQRbgWlx3rj1aNwNMcmGbKTFciDaOYLhTHgU4GlA7ZguydZFDlpVl1VuLnDLzGBcpR7KxP98xNFJEYtuOFS2ORa8P30ncWSCCRH435KdeqCKKvfCMAINHQU4CZkD41p?width=4128&height=3008&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIryQAAAAAANALgH95A9XQ7tg",
 		descr: "It takes 4 steps to un-stow the drone and prepare it for launch. 1) Deploy outriggers; 2) Raise the launcher; 3) Open drone wings; 4) Rotate launcher."
 	},
 	{
 		name: "Drone Take-off",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mmBtma79v1-eDZ2Crn16dUh9MUlB_kl4255b13ei0cPygf6iafbZgC9u21nu3H8VKfz5WANz2ZJ1vBYbYijQh-7_DdroKzEAGzZAWIs9zrjc2QS10zpYX0ZN2y-dgEXDPhgkAHTGjOmEKTe-w7EECE9_SRLJe_Jhe65cfxCBT7bv9fWRLWTZkfKWJabtRWfMK?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIsCQAAAAAANRK5cd_XlJ21jQ",
 		descr: "The drone can be detached from the truck when launched."
 	},
 	{
 		name: "Drone Mechanism",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mWoeCN4oUZb2rQh01G6V26Gg_76lgu16TI8gMf_P43NbkvTnMzw52hpLGW0EW95N8XHKk55bZqMuh3Y2KlIII6_ktom0tHs9pKd-nW8bNIifupuWlWg6S7ozz5Y4m1SMVgrOIykQtVgegM6RbstZBzgXEmMIsEM_KwMQ6yzzZO9TY7j5Sn8au9pKk48_Fv4dz?width=4120&height=3096&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIsiQAAAAAACj8V8sDdmoAgko",
 		descr: "The stowed drone extends its wings for deployment. I used 4-bar linkage mechanisms on both wings."
 	},
 	{
 		name: "Launcher Mechanism",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4myo6ZKGMTixHybG9jXYbuUEHaBnUxAi1wwqhcWUHrqa10sEfd6M9PajZ1j1CWLS2AL5q-u-QxC0tR_gVfO15mP4cq9E4X30l3UrL3EJT0QW5AWIDGzXHpkmdiDZxL5MqIB6MwarH0ryME5M3-6IncNo0AE58XopCANyIJS2z_vULGK-zdYKky7KSUWxwjkd-J?width=4112&height=3096&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIsSQAAAAAAMP9YqpU2uq7g6M",
 		descr: "Mechanism of the launcher that is responsible for raising the drone, rotating it, and extending its wings."
 	},
 	{
 		name: "Gearbox",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4m39vsh4kz5gxreuPq7R5c-conW6G5I2dtG6to_eugZGcx3ab5l4YrOr2RehaJt_dGTfMrMjYjrCW6iR322ZgvIuRuK5JGhggKLdOdIUilv0-lQbneHDWmi9gKNEVJ3-5ie0zUiFQkD1UXnSgYDMtX2cwndVRb3Ipw4i_4RFpxOr_5WpSMX34Jtz51cYfaS4gh?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICItCQAAAAAACn_fMwLU26X90o",
 		descr: "The gearbox that uses the output of a single motor to perform 4 different tasks."
 	},
-    {
+	{
 		name: "Driver",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mqpUNxvupfAVv8KLtPW52t31KTwq0t6QyeGt75J1xH5coPsr6caXD6SpIAOG5HWt0tOsGfUPRb4kFq-2BpM9m2iPZwMKsay0SDaOGcIM7OGwhmgUbv_j-6XGCqSgQL3XoLoffveMIdXgidXh0n0P6YvECZQPD-HgXhwB5LOLIxqk4O0CGo7rb9XnAccNSBC8l?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICItiQAAAAAAPK-iWsGg5S1DNA",
 		descr: "I assume that a truck in 2045 would be autonomous, and the driver can just chill in his seat except for emergency situations."
 	},
-    {
+	{
 		name: "Final",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mfIjB2ZS8KcICzX9BxoThAw_1IPDA7pcuQ5vnIqyL_b44QJylKD4PSa946mOJOK8Mwiwbl367AStHXNPavkbXb03qHyUl7I5d4Q3Knis0gEiPJcxuXStpmt1OZP2b90m8HLBNOR7VDKrLJNcyzLrdZlC5ABUPvLGkc9NNfBLKhDkNBXsI7BR1pKxxOdC446so?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIuCQAAAAAAN50ez87MHuWYtM",
 		descr: "Final view of the truck, with the drone and launcher platform removed."
 	},
 
--- src/pages/towerCranePage.js
+++ src/pages/towerCranePage.js
@@ -4,19 +4,19 @@ import TemplatePage from './templatePage';
 import Icon from '@material-ui/icons/Settings';
 
 function towerCranePage() {
-	
+
 	return (
 		<React.Fragment>
-			<TemplatePage 
-				pageName={pageName} 
-				creationDate={creationDate} 
-				creationDescr={creationDescr} 
-				creationChips={creationChips} 
-				info={info} 
-				icon={<Icon fontSize="large"/>} />
+			<TemplatePage
+				pageName={pageName}
+				creationDate={creationDate}
+				creationDescr={creationDescr}
+				creationChips={creationChips}
+				info={info}
+				icon={<Icon fontSize="large" />} />
 		</React.Fragment>
 	);
-	}
+}
 
 export default towerCranePage;
 
@@ -34,55 +34,55 @@ const info = [
 	{
 		name: "Display",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mDW5Y7sy_o2MF_kAvnH1xGVzoSAoRX2rQN0U7A3DM_BbK5SGvFyf9IYqpVb2DT0B0rH4v3lppVbvwWmhfGFLB05CxR2rUv4Nwi3gI_SG9iBo7cj_Nt1LHKRHJ8WkTGpdozNnvBsjzUauId5wLGTON0nViLGs2_2DHCyL6dhIaAhXb-jCk-6lQrBiRJZAyPJZP?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIoCQAAAAAAN4nnB5YIGBLTQc",
 		descr: "With real tower cranes in the background."
 	},
-    {
+	{
 		name: "Construction in the Background",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mO1ezK7ajfJ7G8fDFRiujOprBvvdMgjWXSGOvVIGFj43k1baSWj_NkZ94aFui5tyWxuO5OyBltT5PUA_jqMWp4pJW3RvfscZj92Jz0eILJ7GHziGLBzOIVwh2Yx1-mTyZ-9VRhPOyuECeSkz8eaU16O9c2JqBMixn18SsGygwc10ziBfvAExW6E_a0t66ktaU?width=4000&height=3000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIoSQAAAAAAImsM8AruZaSfWk",
 		descr: "Zoomed out - this photo was taken from the corridor outside my colleague's apartment. The crane was perched on top of a stool and storage box for this and the previous shot."
 	},
 	{
 		name: "Gearbox",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4myfOvrZhV0SkBsJoOsjYFfPurtrm4foKWaLe_MlenE7rUo8sbcXmGYW2cWcYbpgcYqsm8slhiMb8pjTouy5kWHMZZRZD-mI4EGLZTjEQtLHV1CPtjJ-oIdH9OizG3NmkJkSm2PNkXlNmCKetfVtw7LeP8y9EmyZhRmn5HnbIIJwrwsE5VI8N4KhfX7zjX7WCJ?width=5951&height=3946&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIpCQAAAAAABYNAhAvuDGXzeU",
 		descr: "The red selector is responsible for selecting which part of the crane is moved by the motor - the hook, trolley or jib. It took a lot of trial and error to design an arrangement where the gears and shafts fit within the 5x5 stud mast cross-section."
 	},
 	{
 		name: "Hook Displacement",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mefhHVASPPUrduFSegyGo2Id0mLFJpgCOHb8OtFG4QmzFrLMW3YazGgZULDAsK72DHOeFu9WEnwfUz06xdiZd5JReIH_YNvRkF3U5pFK_e1as0M_mVuTnOE08_gl_jYL7CIVa5Fh4s3kpka1qW22SxZATz_cbP1_bG10i_wL3NMJ8jSOPiP6zKGL5k1H1SOit?width=5624&height=3952&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICInCQAAAAAAF4xWLDUAncvP5k",
 		descr: "The hook is attached to a string, and can be moved up and down."
 	},
 	{
 		name: "Trolley Displacement",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mO-mCzkgw2oG6wmpdH38O_4tV4zSFfPU1YNhr8llwqK4VPNSZXRAPh_L-JoUfabsQvvkb1MmzzEyWz-Gi2xb8Dkg8E5MgzJ2SlUWuBbz81RibQ_xOhLyW8pOn8Jrjku6tjTeY5koz2US92E9V-WB2_bFxGvyL9mNoiPExI4iul7e2-DJ10rqeQjv8DdKnknwT?width=5632&height=3944&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIoiQAAAAAAE6dg5jCfZN6AUc",
 		descr: "The black trolley can be moved along the jib."
 	},
 	{
 		name: "Trolley Mechanism",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mg1D4xbArEwlzpw7BIPubVLlhtOwygi5sQJehOMBVyXkQq1YjqF-t2nqphbnlCFvmMtxhlZpDq7WslxU34aWc0x1lBWZZ4OVHbPxWj9IMYJFdlHs8zkcODjiM0syXx_pv_XJvh5Qv6G5vrJDZy_9OULmuRmY0l7WmDgCjQD9FFFtCmuiziaF9fkqQ3toWV3ZV?width=4952&height=4000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIoyQAAAAAAJre_0PkUCt3NIA",
 		descr: "Movement of the trolley is governed by a series of gears from the gearbox, and a rack and pinion gearset on the jib."
 	},
 	{
 		name: "Jib Rotation",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mVv23qXC_LSNULgS0CvJwEGGWOiRMNlSTvbl5DahwSMTYr65GyF8A2UUMQLz4JjfHZBHxQV6zayFemONtFkO30Wuq-uy9UQSasm9V3U48AN6aXdR2zsF0c4gX0v_8uVolPTtbxpjRR30PYpAnerX4JDCteoAIoODk6yGIwDHi9e9Nc5GRyHKUc2J9qaYGBFUm?width=4496&height=3968&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICImiQAAAAAAPyGSssqlq2jcUI",
 		descr: "The jib can also be rotated."
 	},
 	{
 		name: "Full Length Shot",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mNU96LYCzbMiBt6FxIlCVRIr-3XciAMtBUhJi4gj8cTnY-DOogW2heWkR5zV-Zrg1c7oSKttPR2dAMy2IYtEqCcKWy7VUGlVj_KFeOgM7bI6sS0U-PetS8UxEF5tPYmJKBYzxNOeXvCFOgXbEQrAog0gfY6S8mv5D0ZcNDipVQp_lD5Ftgz2xHdt98CLPrYc-?width=3000&height=4000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICInSQAAAAAAOaVFONYlLkNgPw",
 		descr: "The full length of the tower crane. The heavy battery pack is placed at the bottom for stability, and the driving motor at the bottom of the mast."
 	},
 	{
 		name: "Disassembled",
 		type: "Photo",
-		url: "https://dm2304files.storage.live.com/y4mWVyifuPgsCaEKHlFrRrRQprVTTeY5wcrBktfRCCz-SAh58WQ5-3WhzH4BAc-z4lKFvGBGkymtZQ_2qprOtYIuR1QVHrMJ3VI26LLowmCa7fF3adLWLcPnMaRGfrsHB7cpl76ddw3SybjUSxCpEv3huDerYfDoOmxWumMTvi25Snovhlj2N8i9cXWmaDDayuD?width=3000&height=4000&cropmode=none",
+		url: "https://1drv.ms/i/c/88f9e5f4a517d194/UQSU0Rel9OX5IICIpSQAAAAAAEov1P_9uoUQIz8",
 		descr: "The tower crane can be easily disassembled (this was how I managed to bring it to work)."
 	},
 
